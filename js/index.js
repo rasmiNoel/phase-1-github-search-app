d709db73603200c1259229244a7bc7db35adcd75
rasmi <rasminoel@gmail.com>
2023-10-05 10:57:04 +0300

    Done with assignment

--- js/index.js
+++ js/index.js
@@ -1,46 +1,5 @@
 const init = () => {
 
-    // Obtain list from GitHub API
-    const getUserList = () => {
-        fetch('https://api.github.com/users')
-            .then(response => response.json())
-            .then(users => displayUserList(users))
-
-        // Display Use list
-        const displayUserList = (users) => {
-            const ul = document.querySelector('#user-list');
-
-            users.forEach(user => {
-                const li = document.createElement('li');
-                const a = document.createElement("a");
-                a.href = "index.html?id=repos-list";
-                a.innerText = user.login;
-                li.appendChild(a);
-                ul.appendChild(li);
-            });
-        }
-    }
-
-    // Obtain list from GitHub API  
-    const getReposList = () => {
-        fetch("https://api.github.com/users/mojombo/repos")
-            .then(response => response.json())
-            .then(repos => displayReposList(repos))
-
-        // Display Repos list
-        const displayReposList = (repos) => {
-            const ul = document.querySelector('#repos-list');
-
-            repos.forEach(repo => {
-                const li = document.createElement('li');
-                li.textContent = repo.name;
-                ul.appendChild(li);
-            });
-        }
-    }
-    getUserList();
-    getReposList();
-
     // Form that searchers for users
     const form = document.querySelector('#github-form');
     form.addEventListener('submit', (event) => {
@@ -60,13 +19,37 @@ const init = () => {
             users.items.forEach(user => {
                 const li = document.createElement('li');
                 const a = document.createElement("a");
-                a.href = "index.html?id=repos-list";
+                a.href = `https://api.github.com/search/users?q=${search}`;
                 a.innerText = user.login;
                 li.appendChild(a);
                 ul.appendChild(li);
             });
+
+            // Obtain list from GitHub API  
+            const getReposList = () => {
+                fetch("https://api.github.com/mojombo/repos")
+                    .then(response => response.json())
+                    .then(repos => displayReposList(repos))
+
+                // Display Repos list
+                const displayReposList = (repos) => {
+                    const ul = document.querySelector('#repos-list');
+
+                    repos.forEach(repo => {
+                        const li = document.createElement('li');
+                        li.textContent = repo.name;
+                        ul.appendChild(li);
+                    });
+                }
+            }
+            getReposList();
         }
     });
+
+
+
+
+
 
 
 
